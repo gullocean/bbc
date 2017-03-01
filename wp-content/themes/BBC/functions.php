9d94b8a05ea35530aee8f3989db3f4ba6fe547ca
gullocean <gullocean@gmail.com>
2017-03-01 09:54:09 +0300

    create about us templaet

--- wp-content/themes/BBC/functions.php
+++ wp-content/themes/BBC/functions.php
@@ -84,46 +84,6 @@ endif;
 add_action( 'after_setup_theme', 'bbc_setup' );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function bbc_widgets_init() {
-	register_sidebar( array(
-		                  'id'            => 'sidebar-1',
-		                  'name'          => __( 'Sidebar', 'bbc' ),
-		                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		                  'after_widget'  => '</div>',
-		                  'before_title'  => '<h2 class="widget-title">',
-		                  'after_title'   => '</h2>',
-	                  ) );
-
-	register_sidebar( array(
-		                  'id'            => 'sidebar-home',
-		                  'name'          => __( 'Homepage', 'bbc' ),
-		                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		                  'after_widget'  => '</div>',
-		                  'before_title'  => '<h2 class="widget-title">',
-		                  'after_title'   => '</h2>',
-	                  ) );
-
-	for ( $i = 1; $i < 5; $i ++ ) {
-		register_sidebar( array(
-			                  'id'            => 'footer-widget-' . $i,
-			                  'name'          => sprintf( __( 'Footer Widget %s', 'bbc' ), $i ),
-			                  'description'   => __( 'Used for footer widget area', 'bbc' ),
-			                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
-			                  'after_widget'  => '</div>',
-			                  'before_title'  => '<h2 class="widget-title">',
-			                  'after_title'   => '</h2>',
-		                  ) );
-	}
-
-}
-
-add_action( 'widgets_init', 'bbc_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function bbc_scripts() {
--- wp-content/themes/BBC/page-templates/about-template.php
+++ wp-content/themes/BBC/page-templates/about-template.php
@@ -4,10 +4,18 @@ Template Name: About Template
 */
 get_header(); ?>
 
+
+<?php 
+// Get the custom fields for about template.
+$banner_src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'thumbnail_size' );
+$company_headline = bbc_get_custom_field('company_headline');
+$company_desc = bbc_get_custom_field('company_description');
+
+?>
 <?php $layout_class = ( function_exists( 'bbc_get_layout_class' ) ) ? bbc_get_layout_class() : ''; ?>
 	<div class="row">
-		<div class="banner"></div>
-		<div id="primary" class="col-md-12 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
+		<div class="banner"><img src="<?php echo $banner_src[0]; ?>"></div>
+		<div id="primary" class="col-md-12 mb-xs-24 about-template">
 			<div class="container">
 				<?php
 				while ( have_posts() ) : the_post();
@@ -16,6 +24,79 @@ get_header(); ?>
 
 				endwhile; // End of the loop.
 				?>
+
+				<div class="about-company">
+					<div class="col-md-6 col-xs-12 company-slider">
+						<?php echo do_shortcode('[URIS id=219]'); ?>
+					</div>
+					<div class="col-md-6 col-xs-12 company-desc">
+						<h3><?php echo $company_headline; ?></h3>
+						<p><?php echo $company_desc; ?></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="our-team margin80">
+					<h3>Our Team</h3>
+					<div class="wrapper">
+						<div class="col-md-6 col-xs-12 text-center">
+							<img src="<?php echo bbc_get_custom_field('team_member_1'); ?>" class="portfolio">
+							<div class="social-icons">
+								<a href="https://www.facebook.com/boisebuildingco">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-1.png'?>">
+								</a>
+								<a href="https://twitter.com/BoiseBuildingCo">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-2.png'?>">
+								</a>
+								<a href="http://pinterest.com/">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-3.png'?>">
+								</a>
+								<a href="http://www.linkedin.com/profile/view?id=96482539">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-4.png'?>">
+								</a>
+								<a href="mailto:mike@boisebuilding.co">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-5.png'?>">
+								</a>
+								<a href="/contact">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-6.png'?>">
+								</a>
+							</div>
+							<p class="text-justify"><?php echo bbc_get_custom_field('team_member_1_description'); ?></p>
+						</div>
+						<div class="col-md-6 col-xs-12 text-center">
+							<img src="<?php echo bbc_get_custom_field('team_member_2'); ?>" class="portfolio">
+							<div class="social-icons">
+								<a href="https://www.facebook.com/boisebuildingco">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-1.png'?>">
+								</a>
+								<a href="https://twitter.com/BoiseBuildingCo">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-2.png'?>">
+								</a>
+								<a href="http://pinterest.com/">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-3.png'?>">
+								</a>
+								<a href="http://www.linkedin.com/profile/view?id=96482539">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-4.png'?>">
+								</a>
+								<a href="mailto:mike@boisebuilding.co">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-5.png'?>">
+								</a>
+								<a href="/contact">
+									<img src="<?php echo get_template_directory_uri() . '/assets/images/social_icons/t-6.png'?>">
+								</a>
+							</div>
+							<p class="text-justify"><?php echo bbc_get_custom_field('team_member_2_description'); ?></p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="divider"></div>
+				<div class="image-group">
+					<div class="col-md-3 col-xs-6 text-center"><img src="<?php echo bbc_get_custom_field('item1'); ?>"></div>
+					<div class="col-md-3 col-xs-6 text-center"><img src="<?php echo bbc_get_custom_field('item2'); ?>"></div>
+					<div class="col-md-3 col-xs-6 text-center"><img src="<?php echo bbc_get_custom_field('item3'); ?>"></div>
+					<div class="col-md-3 col-xs-6 text-center"><img src="<?php echo bbc_get_custom_field('item4'); ?>"></div>
+					<div class="clearfix"></div>
+				</div>
 			</div>
 		</div><!-- #primary -->
 	</div>
