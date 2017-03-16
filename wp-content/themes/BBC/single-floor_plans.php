a88838d7bdd642e4986773c085a6bc1237d91933
gullocean <gullocean@gmail.com>
2017-03-16 12:15:28 +0300

    upload floor plans contents

--- wp-content/themes/BBC/single-floor_plans.php
+++ wp-content/themes/BBC/single-floor_plans.php
@@ -6,7 +6,7 @@
 
 get_header(); 
 
-$thumb_src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'thumbnail_size' );
+//$thumb_src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'thumbnail_size' );
 ?>
 <?php $layout_class = ( function_exists( 'bbc_get_layout_class' ) ) ? bbc_get_layout_class() : ''; ?>
 	<div class="row floor-plan-custom-post">
@@ -20,7 +20,7 @@ $thumb_src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), '
 				</div>
 			</div>
 			<div class="col-md-8 col-xs-12 plan-picture">
-				<img src="<?php echo $thumb_src[0]; ?>">
+				<?php echo do_shortcode('[URIS id=' . bbc_get_custom_field('slider_id') . ']'); ?>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -36,17 +36,21 @@ $thumb_src = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), '
 						endwhile; // End of the loop. 
 					?>
 
-					<?php bbc_display_social_icons(); ?>
+					<?php 
+						$current_page_url = get_permalink();
+						$property_name = get_the_title();
+						bbc_display_social_icons($current_page_url, $property_name); 
+					?>
 				</div>
 				<div class="col-md-6 col-xs-12 virtual-tour">
 					<h1>Virtual Tour</h1>
-					<div class="youtube"><iframe src="<?php echo get_post_meta(get_the_ID(), 'virtual_tour', true); ?>" width="100%" height="300"></iframe></div>
+					<div class="youtube"><iframe width="100%" height="300" src="<?php echo get_post_meta(get_the_ID(), 'virtual_tour', true); ?>"></iframe></div>
 
 					<h1>Floor Plans</h1>
 					<div class="plans-panel">
 						<ul class="nav nav-pills">
-							<li class="active"><a  href="#1st" data-toggle="tab">1st Floor</a></li>
-							<li><a href="#2nd" data-toggle="tab">2nd Floor</a></li>
+							<li class="active"><a  href="#1st" data-toggle="tab">Elevation</a></li>
+							<li><a href="#2nd" data-toggle="tab">Floor Plan</a></li>
 							<li><a href="#details" data-toggle="tab">Details</a></li>
 						</ul>
 						<div class="tab-content clearfix">
