f7ab00dd7077c2107a2af41b2f7388c1b61f8b48
gullocean <gullocean@gmail.com>
2017-03-31 21:50:43 +0300

    display associated properties to each community post page

--- wp-content/themes/BBC/single-community.php
+++ wp-content/themes/BBC/single-community.php
@@ -1,15 +1,26 @@
 <?php
-get_header();
-// Get the custom fields for community post template.
-$location = get_post_meta(get_the_ID(), 'location', true);
+	get_header();
+	// Get the custom fields for community post template.
+	$location = get_post_meta(get_the_ID(), 'location', true);
 
-$img_group = array();
-for ($i=1; $i < 4; $i++) { 
-	$gallery_img_src = bbc_get_custom_field('community_gallery' . $i);
-	if (!empty($gallery_img_src)) {
-		array_push($img_group, $gallery_img_src);
+	$img_group = array();
+	for ($i=1; $i < 4; $i++) { 
+		$gallery_img_src = bbc_get_custom_field('community_gallery' . $i);
+		if (!empty($gallery_img_src)) {
+			array_push($img_group, $gallery_img_src);
+		}
 	}
-}
+
+
+	// Get the checked properties in each community post.
+	$terms = wp_get_post_terms( get_the_ID(), 'associated_properties', array("fields" => "all") );
+
+	$associated_post_ids = [];
+	foreach ($terms as $term) {
+		$property_name = $term->name;
+		$post_id = get_page_by_title( $property_name, 'OBJECT', 'property' );
+		$associated_post_ids[] = $post_id->ID;
+	}
 
 ?>
 	<div class="row">
@@ -45,35 +56,26 @@ for ($i=1; $i < 4; $i++) {
 				</div>
 
 				<!-- Display the properties associated to each community. -->
+				<?php if (!empty($associated_post_ids)): ?>
 				<div class="associated-post">
 					<h2 class="text-center post-title">Here are associated properties.</h2>
 					<?php
-						// Get the checked properties in each community post.
-						$terms = wp_get_post_terms( get_the_ID(), 'associated_properties', array("fields" => "all") );
 
-						$associated_post_ids = [];
-						foreach ($terms as $term) {
-							$property_name = $term->name;
-							$post_id = get_page_by_title( $property_name, 'OBJECT', 'property' );
-							$associated_post_ids[] = $post_id->ID;
-						}
-
-						if (!empty($associated_post_ids)) {
-							foreach ($associated_post_ids as $associated_post_id) {
-								$post_title = get_the_title($associated_post_id);
-								$featured_img_src = wp_get_attachment_image_src( get_post_thumbnail_id($associated_post_id), 'thumbnail_size' );
-							?>
-							<div class="col-md-4 col-xs-12 text-center">
-								<a href="<?php echo get_permalink($associated_post_id); ?>"><img src="<?php echo $featured_img_src[0]; ?>"></a>
-								<h3 class="post-title"><?php echo $post_title; ?></h3>
-							</div>
-							<?php
-							}
+						foreach ($associated_post_ids as $associated_post_id) {
+							$post_title = get_the_title($associated_post_id);
+							$featured_img_src = wp_get_attachment_image_src( get_post_thumbnail_id($associated_post_id), 'thumbnail_size' );
+						?>
+						<div class="col-md-4 col-xs-12 text-center">
+							<a href="<?php echo get_permalink($associated_post_id); ?>"><img src="<?php echo $featured_img_src[0]; ?>"></a>
+							<h3 class="post-title"><?php echo $post_title; ?></h3>
+						</div>
+						<?php
 						}
 
 					?>
 					<div class="clearfix"></div>
 				</div>
+				<?php endif; ?>
 			</div>
 		</div><!-- #primary -->
 
